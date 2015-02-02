2d847c54b7b2d5a406466e91ae1b1229ee0bf98e
James Douglass <jdouglass@stanford.edu>
2015-02-01 22:25:33 -0800

    Modifying the test for the package version number
    When the package is in the source tree, there is no version, since it
    can be fetched from <repo root>/versioning.py.  When we're in a
    distribution (like when it's published to pypi or installed in a
    virtualenv), the version information is present.
    Branch: develop

--- natcap/geoprocessing/tests/smoke_test.py
+++ natcap/geoprocessing/tests/smoke_test.py
@@ -1,7 +1,14 @@
 import unittest
 import natcap.geoprocessing
+import os
 
 class TestImportPackage(unittest.TestCase):
     def test_import_package(self):
-        # assert that the version was set correctly on distribution.
-        self.assertNotEqual(natcap.geoprocessing.__version__, 'dev')
+        print
+        print os.getcwd()
+        if 'site-packages' in natcap.geoprocessing.__file__:
+            # assert that the version was set correctly on distribution.
+            self.assertNotEqual(natcap.geoprocessing.__version__, 'dev')
+        else:
+            get_version = lambda: natcap.geoprocessing.__version__
+            self.assertRaises(AttributeError, get_version)
