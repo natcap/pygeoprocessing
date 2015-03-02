5abe70f4e892c33557145ccee2c6999f974c8060
Richard Sharp <richpsharp@gmail.com>
2015-03-02 14:15:21 -0800

    issue #1 adding virtual env script to run tests on windows. Branch: feature/raster-routing-utils-to-pygeoprocessing

---
+++ test_suite_windows.bat
@@ -0,0 +1,18 @@
+SET ENVDIR=pygeoprocessing_virtualenv
+python bootstrap_invest_environment.py > setup_environment.py
+
+IF "%1"== "clean" GOTO CLEANBUILD
+
+python setup_environment.py --system-site-packages %ENVDIR%
+GOTO BULIDDONE
+
+:CLEANBUILD
+DEL /S /Q build
+DEL /S /Q %ENVDIR%
+python setup_environment.py --clear --system-site-packages %ENVDIR%
+GOTO BULIDDONE
+
+:BULIDDONE
+copy C:\Python27\Lib\distutils\distutils.cfg .\%ENVDIR%\Lib\distutils\distutils.cfg
+%ENVDIR%\Scripts\python setup.py install
+%ENVDIR%\Scripts\nosetests -vs --nologcapture
