8f30af7d1b697be154116bd6ee3fc088fab96c77
Richard Sharp <richpsharp@gmail.com>
2015-04-14 10:28:14 -0700

    removing a custom OrderDict since one exists in collections Branch: feature/nodata_byte_raster_refactor

--- pygeoprocessing/geoprocessing.py
+++ pygeoprocessing/geoprocessing.py
@@ -69,38 +69,6 @@ class SpatialExtentOverlapException(Exception):
         in space"""
     pass
 
-from UserDict import DictMixin
-
-
-class OrderedDict(DictMixin):
-    """A python dictionary that has fast index and preserves order"""
-
-    def __init__(self):
-        self._keys = []
-        self._data = {}
-
-    def __setitem__(self, key, value):
-        if key not in self._data:
-            self._keys.append(key)
-        self._data[key] = value
-
-    def __getitem__(self, key):
-        return self._data[key]
-
-    def __delitem__(self, key):
-        del self._data[key]
-        self._keys.remove(key)
-
-    def keys(self):
-        return list(self._keys)
-
-    def copy(self):
-        copyDict = odict()
-        copyDict._data = self._data.copy()
-        copyDict._keys = self._keys[:]
-        return copyDict
-
-
 logging.basicConfig(format='%(asctime)s %(name)-18s %(levelname)-8s \
     %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
 
