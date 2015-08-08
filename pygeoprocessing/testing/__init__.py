51d10f900e91f73cdc0643a605c7635412452edb
James Douglass <jdouglass@stanford.edu>
2015-08-07 19:09:33 -0700

    Exposing more functions and classes at the testing package level. Branch: feature/geospatial-testing

--- pygeoprocessing/testing/__init__.py
+++ pygeoprocessing/testing/__init__.py
@@ -1,8 +1,22 @@
-from assertions import *
-from utils import get_hash
+from assertions import assert_equal, assert_almost_equal, \
+    assert_rasters_equal, assert_vectors_equal, assert_csv_equal, \
+    assert_md5, assert_matrixes, assert_archives, assert_workspace, \
+    assert_json, assert_text_equal, assert_files, assert_snapshot
+
+from utils import get_hash, save_workspace, regression, \
+    build_regression_archives, snapshot_folder
+from sampledata import raster, vector, RasterFactory, VectorFactory
 
 __all__ = [
+    'raster',
+    'vector',
+    'RasterFactory',
+    'VectorFactory',
     'get_hash',
+    'save_workspace',
+    'regression',
+    'build_regression_archives',
+    'snapshot_folder',
     'assert_equal',
     'assert_almost_equal',
     'assert_rasters_equal',
