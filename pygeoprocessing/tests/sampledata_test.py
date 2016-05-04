1bdc6a040f9660a49d07c2a7cb710ed6f7f0cebe
James Douglass <jdouglass@stanford.edu>
2016-05-03 21:06:36 -0700

    Adding a couple last tests to achieve 100% coverage of testing.
    RE:#BITBUCKET-42.
    Branch: bugfix/TESTING-42-complete-test-coverage

--- pygeoprocessing/tests/sampledata_test.py
+++ pygeoprocessing/tests/sampledata_test.py
@@ -1,11 +1,14 @@
 import unittest
 import os
+import subprocess
+import shutil
 
 from osgeo import gdal
 from osgeo import ogr
 from osgeo import osr
 import numpy
 from shapely.geometry import Polygon
+import mock
 
 import pygeoprocessing
 import pygeoprocessing.testing
@@ -130,6 +133,29 @@ class RasterTest(unittest.TestCase):
         set_nodata_value = pygeoprocessing.get_nodata_from_uri(filename)
         self.assertEqual(set_nodata_value, None)
 
+    def test_raster_bad_matrix_iterable_input(self):
+        """Verify TypeError raised when band_matrices not a list."""
+        pixels = set([1])
+        nodata = None
+        reference = sampledata.SRS_WILLAMETTE
+        filename = pygeoprocessing.temporary_filename()
+        with self.assertRaises(TypeError):
+            sampledata.create_raster_on_disk(
+                pixels, reference.origin, reference.projection, nodata,
+                reference.pixel_size(30), datatype='auto', filename=filename)
+
+    def test_raster_multiple_dtypes(self):
+        """Verify TypeError raised when matrix band dtypes are mismatched."""
+        pixels = [numpy.array([[0]], dtype=numpy.int),
+                  numpy.array([[0]], dtype=numpy.float)]
+        nodata = None
+        reference = sampledata.SRS_WILLAMETTE
+        filename = pygeoprocessing.temporary_filename()
+        with self.assertRaises(TypeError):
+            sampledata.create_raster_on_disk(
+                pixels, reference.origin, reference.projection, nodata,
+                reference.pixel_size(30), datatype='auto', filename=filename)
+
 
 class VectorTest(unittest.TestCase):
     def test_init(self):
@@ -168,3 +194,37 @@ class VectorTest(unittest.TestCase):
         self.assertRaises(AssertionError, sampledata.create_vector_on_disk, [],
                           sampledata.SRS_WILLAMETTE.projection,
                           vector_format='foobar')
+
+class GISBrowserTest(unittest.TestCase):
+
+    """Test fixture for opening a default GIS browser."""
+
+    def test_qgis_called(self):
+        """Verify QGIS is called with the correct CLI args."""
+        from pygeoprocessing.testing.sampledata import \
+            open_files_in_gis_browser
+        file_list = ['foo']
+        with mock.patch('subprocess.call'):
+            open_files_in_gis_browser(file_list)
+            self.assertTrue(subprocess.call.called)
+            self.assertEqual(subprocess.call.call_args[0][0],
+                             ['qgis'] + file_list)
+
+
+class CleanupTest(unittest.TestCase):
+
+    """Test fixture for file cleanup functionality."""
+
+    def test_cleanup_dir(self):
+        """Verify shutil.rmtree is called by cleanup()."""
+        from pygeoprocessing.testing.sampledata import cleanup
+        with mock.patch('shutil.rmtree'):
+            cleanup('/')
+            self.assertTrue(shutil.rmtree.called)
+
+    def test_cleanup_file(self):
+        """Verify os.remove is called by cleanup()."""
+        from pygeoprocessing.testing.sampledata import cleanup
+        with mock.patch('os.remove'):
+            cleanup('/foo')
+            self.assertTrue(os.remove.called)
