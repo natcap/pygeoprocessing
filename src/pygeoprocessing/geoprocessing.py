354ae7d709b8fbf822a36e49f5fe42876568668a
Rich Sharp <richpsharp@gmail.com>
2016-07-22 15:42:33 -0700

    re #BITBUCKET-107 addressing issue of _next_regular in future scipy versions by directly nesting code in this project. Branch: develop

--- src/pygeoprocessing/geoprocessing.py
+++ src/pygeoprocessing/geoprocessing.py
@@ -2981,6 +2981,65 @@ def distance_transform_edt(
     except OSError:
         LOGGER.warn("couldn't remove file %s", mask_as_byte_uri)
 
+
+def _next_regular(target):
+    """
+    Find the next regular number greater than or equal to target.
+
+    Regular numbers are composites of the prime factors 2, 3, and 5.
+    Also known as 5-smooth numbers or Hamming numbers, these are the optimal
+    size for inputs to FFTPACK.
+
+    This source was taken directly from scipy.signaltools and saves us from
+    having to access a protected member in a library that could change in
+    future releases:
+
+    https://github.com/scipy/scipy/blob/v0.17.1/scipy/signal/signaltools.py#L211
+
+    Parameters:
+        target (int): a positive integer to start to find the next Hamming
+            number.
+
+    Returns:
+        The next regular number greater than or equal to `target`.
+    """
+    if target <= 6:
+        return target
+
+    # Quickly check if it's already a power of 2
+    if not (target & (target-1)):
+        return target
+
+    match = float('inf')  # Anything found will be smaller
+    p5 = 1
+    while p5 < target:
+        p35 = p5
+        while p35 < target:
+            # Ceiling integer division, avoiding conversion to float
+            # (quotient = ceil(target / p35))
+            quotient = -(-target // p35)
+
+            # Quickly find next power of 2 >= quotient
+            p2 = 2**((quotient - 1).bit_length())
+
+            N = p2 * p35
+            if N == target:
+                return N
+            elif N < match:
+                match = N
+            p35 *= 3
+            if p35 == target:
+                return p35
+        if p35 < match:
+            match = p35
+        p5 *= 5
+        if p5 == target:
+            return p5
+    if p5 < match:
+        match = p5
+    return match
+
+
 def convolve_2d_uri(signal_path, kernel_path, output_path):
     """Convolve 2D kernel over 2D signal.
 
@@ -3089,8 +3148,7 @@ def convolve_2d_uri(signal_path, kernel_path, output_path):
                 numpy.array(kernel_block.shape) - 1)
 
             # add zero padding so FFT is fast
-            fshape = [
-                scipy.signal.signaltools._next_regular(int(d)) for d in shape]
+            fshape = [_next_regular(int(d)) for d in shape]
 
             kernel_fft = numpy.fft.rfftn(kernel_block, fshape)
             signal_fft = _fft_cache(
