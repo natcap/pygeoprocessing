dc947bfa692a5894a4d91aff813727c3091a5bd3
Rich Sharp <richpsharp@gmail.com>
2016-10-07 14:34:42 -0700

    re #BITBUCKET-63 adding regression test for aggregate raster function Branch: feature/GEOPROCESSING-63-aggregate_raster_test_iterblocks

--- tests/test_pygeoprocessing.py
+++ tests/test_pygeoprocessing.py
@@ -1,4 +1,5 @@
 """Module to test PyGeoprocessing module."""
+import collections
 import unittest
 import os
 import tempfile
@@ -139,3 +140,75 @@ class PyGeoprocessingTest(unittest.TestCase):
 
         pygeoprocessing.testing.assertions.assert_rasters_equal(
             output_path, expected_output_path, 1e-6)
+
+    @scm.skip_if_data_missing(TEST_DATA)
+    def test_aggregate_raster_values(self):
+        """PyGeoprocessing: test aggregate raster values."""
+        import pygeoprocessing
+
+        base_raster_path = os.path.join(
+            TEST_DATA, 'aggregate_raster_values_data', 'base_raster.tif')
+
+        shapefile_path = os.path.join(
+            TEST_DATA, 'aggregate_raster_values_data', 'watershed.shp')
+
+        result = pygeoprocessing.aggregate_raster_values_uri(
+            base_raster_path, shapefile_path, shapefile_field='DN',
+            all_touched=False, polygons_might_overlap=True)
+
+        aggregated_values_type = collections.namedtuple(
+            'AggregatedValues',
+            'total pixel_mean hectare_mean n_pixels pixel_min pixel_max')
+
+        expected_result = aggregated_values_type(
+            total={1: 3.0, 2: 398425.0},
+            pixel_mean={1: 1.0, 2: 1.0},
+            hectare_mean={1: 11.111111110950143, 2: 11.111111110937156},
+            n_pixels={1: 3.0, 2: 398425.0},
+            pixel_min={1: 1.0, 2: 1.0},
+            pixel_max={1: 1.0, 2: 1.0}
+        )
+
+        for metric in [
+                'total', 'pixel_mean', 'hectare_mean', 'n_pixels',
+                'pixel_min', 'pixel_max']:
+            _assert_deep_almost_equal(
+                self, getattr(expected_result, metric),
+                getattr(result, metric), places=6)
+
+
+def _assert_deep_almost_equal(test_case, expected, actual, *args, **kwargs):
+    """Assert that two complex structures have almost equal contents.
+
+    I ripped this from this stackoverflow post:
+    http://stackoverflow.com/a/23550280/42897
+
+    Compares lists, dicts and tuples recursively. Checks numeric values
+    using test_case's :py:meth:`unittest.TestCase.assertAlmostEqual` and
+    checks all other values with :py:meth:`unittest.TestCase.assertEqual`.
+    Accepts additional positional and keyword arguments and pass those
+    intact to assertAlmostEqual() (that's how you specify comparison
+    precision).
+
+    :param test_case: TestCase object on which we can call all of the basic
+    'assert' methods.
+    :type test_case: :py:class:`unittest.TestCase` object
+    """
+    is_root = '__trace' not in kwargs
+    trace = kwargs.pop('__trace', 'ROOT')
+    if isinstance(expected, (int, float, long, complex)):
+        test_case.assertAlmostEqual(expected, actual, *args, **kwargs)
+    elif isinstance(expected, (list, tuple, numpy.ndarray)):
+        test_case.assertEqual(len(expected), len(actual))
+        for index in xrange(len(expected)):
+            v1, v2 = expected[index], actual[index]
+            _assert_deep_almost_equal(
+                test_case, v1, v2, __trace=repr(index), *args, **kwargs)
+    elif isinstance(expected, dict):
+        test_case.assertEqual(set(expected), set(actual))
+        for key in expected:
+            _assert_deep_almost_equal(
+                test_case, expected[key], actual[key], __trace=repr(key),
+                *args, **kwargs)
+    else:
+        test_case.assertEqual(expected, actual)
