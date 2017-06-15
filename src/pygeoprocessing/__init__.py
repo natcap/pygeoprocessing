d775e97fe87f518c2c9fbfd6e823e52a4eb3d576
James Douglass <jdouglass@stanford.edu>
2017-06-15 15:35:53 -0700

    Exposing calculate_slope.  RE:#BITBUCKET-78 Branch: bugfix/IMPORTS-78-from-pygeoprocessing-import-star

--- src/pygeoprocessing/__init__.py
+++ src/pygeoprocessing/__init__.py
@@ -10,11 +10,13 @@ import types
 import sys
 
 from . import geoprocessing
+from .geoprocessing_core import calculate_slope
 
 LOGGER = logging.getLogger('pygeoprocessing')
 LOGGER.setLevel(logging.DEBUG)
 
-__all__ = []
+
+__all__ = ['calculate_slope']
 for attrname in dir(geoprocessing):
     attribute = getattr(geoprocessing, attrname)
     if type(attribute) is types.FunctionType:
