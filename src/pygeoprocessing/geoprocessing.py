re #BITBUCKET-123 agressively functionizing some loops in disjoint polygon calculation Branch: bugfix/GEOPROCESSING-123-optimize-reproject-vector

src/pygeoprocessing/geoprocessing.py
@@ -20,6 +20,7 @@ import distutils.version
 import multiprocessing
 import multiprocessing.pool
 import threading
+import collections
 
 try:
     import queue
@@ -54,7 +55,7 @@ import scipy.sparse
 import scipy.signal
 import scipy.ndimage
 import scipy.signal.signaltools
-import shapely.wkt
+import shapely.wkb
 import shapely.ops
 import shapely.prepared
 from . import geoprocessing_core
@@ -1794,62 +1795,91 @@ def calculate_disjoint_polygon_set(vector_path, layer_index=0):
     """
     vector = gdal.OpenEx(vector_path)
     vector_layer = vector.GetLayer(layer_index)
+    feature_count = vector_layer.GetFeatureCount()
 
-    poly_rtree_index = rtree.index.Index()
-    poly_intersect_lookup = {}
-    for poly_feat in vector_layer:
-        poly_wkt = poly_feat.GetGeometryRef().ExportToWkt()
-        shapely_polygon = shapely.wkt.loads(poly_wkt)
-        poly_rtree_index.insert(
-            poly_feat.GetFID(), shapely_polygon.bounds)
-        poly_wkt = None
-        poly_fid = poly_feat.GetFID()
-        poly_intersect_lookup[poly_fid] = {
-            'poly': shapely_polygon,
-            'intersects': set(),
-        }
+    last_time = time.time()
+    LOGGER.info("build shapely polygon list")
+
+    shapely_polygon_list = [
+        (poly_feat.GetFID(),
+         shapely.wkb.loads(poly_feat.GetGeometryRef().ExportToWkb()))
+        for poly_feat in vector_layer]
+
+    LOGGER.info("build shapely rtree index")
+    poly_rtree_index = rtree.index.Index(
+        [(poly_fid, poly.bounds, None)
+         for poly_fid, poly in shapely_polygon_list])
+
     vector_layer = None
     vector = None
+    LOGGER.info(
+        'poly feature lookup 100.0%% complete on %s',
+        os.path.basename(vector_path))
 
-    for poly_fid in poly_intersect_lookup:
-        current_poly = poly_intersect_lookup[poly_fid]['poly']
-        polygon = shapely.prepared.prep(current_poly)
-        for intersect_poly_fid in poly_rtree_index.intersection(
-                current_poly.bounds):
+    LOGGER.info('build poly intersection lookup')
+    poly_intersect_lookup = collections.defaultdict(set)
+    for poly_index, (poly_fid, poly_geom) in enumerate(shapely_polygon_list):
+        last_time = _invoke_timed_callback(
+            last_time, lambda: LOGGER.info(
+                "poly intersection lookup approximately %.1f%% complete "
+                "on %s", 100.0 * float(poly_index+1) / len(
+                    shapely_polygon_list), os.path.basename(vector_path)),
+            _LOGGING_PERIOD)
+        possible_intersection_set = poly_rtree_index.intersection(
+            poly_geom.bounds)
+        if not possible_intersection_set:
+            continue
+        polygon = shapely.prepared.prep(poly_geom)
+        for intersect_poly_fid in possible_intersection_set:
             if intersect_poly_fid == poly_fid or polygon.intersects(
-                    poly_intersect_lookup[intersect_poly_fid]['poly']):
-                poly_intersect_lookup[poly_fid]['intersects'].add(
-                    intersect_poly_fid)
+                    shapely_polygon_list[intersect_poly_fid][1]):
+                poly_intersect_lookup[poly_fid].add(intersect_poly_fid)
         polygon = None
+    LOGGER.info(
+        'poly intersection feature lookup 100.0%% complete on %s',
+        os.path.basename(vector_path))
 
     # Build maximal subsets
     subset_list = []
     while len(poly_intersect_lookup) > 0:
+        last_time = _invoke_timed_callback(
+            last_time, lambda: LOGGER.info(
+                "maximal subset build approximately %.1f%% complete "
+                "on %s", 100.0 * float(
+                    feature_count - len(poly_intersect_lookup)) /
+                feature_count, os.path.basename(vector_path)),
+            _LOGGING_PERIOD)
+
         # sort polygons by increasing number of intersections
-        heap = []
-        for poly_fid, poly_dict in poly_intersect_lookup.items():
-            heapq.heappush(
-                heap, (len(poly_dict['intersects']), poly_fid, poly_dict))
+        intersections_list = [
+            (len(poly_intersect_set), poly_fid, poly_intersect_set)
+            for poly_fid, poly_intersect_set in
+            poly_intersect_lookup.items()]
+        intersections_list.sort()
 
         # build maximal subset
         maximal_set = set()
-        while len(heap) > 0:
-            _, poly_fid, poly_dict = heapq.heappop(heap)
-            for maxset_fid in maximal_set:
-                if maxset_fid in poly_intersect_lookup[poly_fid]['intersects']:
-                    # it intersects and can't be part of the maximal subset
-                    break
-            else:
-                # made it through without an intersection, add poly_fid to
-                # the maximal set
+        for _, poly_fid, poly_intersect_set in intersections_list:
+            last_time = _invoke_timed_callback(
+                last_time, lambda: LOGGER.info(
+                    "maximal subset build approximately %.1f%% complete "
+                    "on %s", 100.0 * float(
+                        feature_count - len(poly_intersect_lookup)) /
+                    feature_count, os.path.basename(vector_path)),
+                _LOGGING_PERIOD)
+            if not poly_intersect_set.intersection(maximal_set):
+                # no intersection, add poly_fid to the maximal set and remove
+                # the polygon from the lookup
                 maximal_set.add(poly_fid)
-                # remove that polygon and update the intersections
                 del poly_intersect_lookup[poly_fid]
         # remove all the polygons from intersections once they're computed
-        for maxset_fid in maximal_set:
-            for poly_dict in poly_intersect_lookup.values():
-                poly_dict['intersects'].discard(maxset_fid)
+        for poly_fid, poly_intersect_set in poly_intersect_lookup.items():
+            poly_intersect_lookup[poly_fid] = (
+                poly_intersect_set.difference(maximal_set))
         subset_list.append(maximal_set)
+    LOGGER.info(
+        'maximal subset build 100.0%% complete on %s',
+        os.path.basename(vector_path))
     return subset_list
 
 