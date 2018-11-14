3965e581d0dbb816c76d2a0c2c7895a39697d067
James Douglass <jdouglass@stanford.edu>
2018-11-14 13:28:07 -0800

    Removing unneded compiler directives.  RE:#BITBUCKET-130 Branch: feature/ROUTING-130-implement-d8-watershed-delineation-nested

--- setup.py
+++ setup.py
@@ -75,9 +75,6 @@ setup(
                  'src/pygeoprocessing/geoprocessing_core.pyx'],
              include_dirs=[numpy.get_include()],
              language="c++")],
-        compiler_directives={
-            'linetrace': True,
-            'binding': True,
-            'language_level': 2},
+        compiler_directives={'language_level': 2},
         )
 )
