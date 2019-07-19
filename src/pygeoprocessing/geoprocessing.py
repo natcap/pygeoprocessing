3f88a7a595d88cf2e5a8c1aa4ad0b581144efe0d
Rich Sharp <richpsharp@gmail.com>
2019-07-19 10:48:05 -0700

    numpy reports indexing must be a tuple not a sequence so casting to tuple Branch: feature/GEOPROCESSING-183-reclassify-raster-gtiff-options

--- src/pygeoprocessing/geoprocessing.py
+++ src/pygeoprocessing/geoprocessing.py
@@ -408,7 +408,8 @@ def raster_calculator(
                                 blocksize[dim_index],)
                             tile_dims[dim_index] = 1
                     data_blocks.append(
-                        numpy.tile(tuple(value[slice_list]), tile_dims))
+                        numpy.tile(
+                            tuple(value[tuple(slice_list)]), tile_dims))
                 else:
                     # must be a raw tuple
                     data_blocks.append(value[0])
