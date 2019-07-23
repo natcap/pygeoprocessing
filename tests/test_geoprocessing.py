bfe94e7aa806f11c1337689c6b9fa45fd9ad33fc
Rich Sharp <richpsharp@gmail.com>
2019-07-23 16:37:58 -0700

    re #BITBUCKET-182 adding three tests for the three possible types of percentile rasters Branch: feature/GEOPROCESSING-182-c-percentile-implementation

--- tests/test_geoprocessing.py
+++ tests/test_geoprocessing.py
@@ -3737,21 +3737,21 @@ class PyGeoprocessing10(unittest.TestCase):
         actual_message = str(cm.exception)
         self.assertTrue(expected_message in actual_message, actual_message)
 
-    def test_percentile(self):
-        """PGP: test percentile function."""
+    def test_percentile_long_type(self):
+        """PGP: test percentile with long type."""
         srs = osr.SpatialReference()
         srs.ImportFromEPSG(4326)
         gtiff_driver = gdal.GetDriverByName('GTiff')
-        raster_path = os.path.join(self.workspace_dir, 'small_raster.tif')
+        int_raster_path = os.path.join(self.workspace_dir, 'int_raster.tif')
         n_length = 10
-        new_raster = gtiff_driver.Create(
-            raster_path, n_length, n_length, 1, gdal.GDT_Int32, options=[
+        int_raster = gtiff_driver.Create(
+            int_raster_path, n_length, n_length, 1, gdal.GDT_Int32, options=[
                 'TILED=YES', 'BIGTIFF=YES', 'COMPRESS=LZW',
                 'BLOCKXSIZE=16', 'BLOCKYSIZE=16'])
-        new_raster.SetProjection(srs.ExportToWkt())
-        new_raster.SetGeoTransform([0.0, 1.0, 0.0, 0.0, 0.0, -1.0])
-        new_band = new_raster.GetRasterBand(1)
-        new_band.SetNoDataValue(-1)
+        int_raster.SetProjection(srs.ExportToWkt())
+        int_raster.SetGeoTransform([0.0, 1.0, 0.0, 0.0, 0.0, -1.0])
+        int_band = int_raster.GetRasterBand(1)
+        int_band.SetNoDataValue(-1)
         # I made this array from a random set and since it's 100 elements long
         # I know exactly the percentile cutoffs.
         array = numpy.array([
@@ -3770,18 +3770,109 @@ class PyGeoprocessing10(unittest.TestCase):
             8226559, 8355570, 8433741, 8523959, 8853540, 8999076, 9109444,
             9250199, 9262560, 9365311, 9404229, 9529068, 9597598,
             9715208])
-        new_band.WriteArray(array.reshape((n_length, n_length)))
-        new_raster.FlushCache()
-        new_band = None
-        new_raster = None
+        int_band.WriteArray(array.reshape((n_length, n_length)))
+        int_raster.FlushCache()
+        int_band = None
+        int_raster = None
 
         percentile_cutoffs = [0.0, 22.5, 72.1, 99.0, 100.0]
         # manually rounding up the percentiles
-        expected_percentiles = [
+        expected_int_percentiles = [
             array[0], array[23], array[73], array[99], array[99]]
-
-        actual_percentiles = pygeoprocessing.disk_based_percentile(
-            (raster_path, 1), self.workspace_dir, percentile_cutoffs)
-
+        actual_int_percentiles = pygeoprocessing.disk_based_percentile(
+            (int_raster_path, 1), self.workspace_dir, percentile_cutoffs)
         numpy.testing.assert_almost_equal(
-            actual_percentiles, expected_percentiles)
+            actual_int_percentiles, expected_int_percentiles)
+
+    def test_percentile_double_type(self):
+        """PGP: test percentile function with double type."""
+        srs = osr.SpatialReference()
+        srs.ImportFromEPSG(4326)
+        gtiff_driver = gdal.GetDriverByName('GTiff')
+        percentile_cutoffs = [0.0, 22.5, 72.1, 99.0, 100.0]
+
+        array = numpy.array([
+            0.003998113607125986, 0.012483605193988612, 0.015538926080136628,
+            0.0349541783138948, 0.056811563936455145, 0.06472245939357957,
+            0.06763766500876733, 0.0996146617328485, 0.10319174490493743,
+            0.1108529662149651, 0.11748524088704182, 0.13932099810203546,
+            0.14593634331220395, 0.17290496444623904, 0.18410163405268687,
+            0.19228618118906593, 0.19472498766411306, 0.19600894348473485,
+            0.19675234705931377, 0.22294217186343712, 0.24301516135472034,
+            0.25174824011708297, 0.25961212269403156, 0.2633977735981743,
+            0.26432729041437664, 0.26786682579209775, 0.29237261233784806,
+            0.29255695849184316, 0.2964090298109583, 0.2990174003705779,
+            0.30405728527347686, 0.3264688470028486, 0.33514871371769506,
+            0.3482838254608601, 0.35647966026887656, 0.3610103066480047,
+            0.36266883466382505, 0.3722525921166677, 0.3732773924434396,
+            0.37359492466545774, 0.3782442911035093, 0.38183103184230927,
+            0.4061775627324341, 0.40752141481722104, 0.42563138319552407,
+            0.45240943914984344, 0.48131663894772847, 0.48452027730035463,
+            0.5080370178708488, 0.5160581721673511, 0.5207327746991738,
+            0.5218827923543758, 0.5254400558377796, 0.5314284222888134,
+            0.5399960806407419, 0.5540251419037007, 0.567883875779636,
+            0.5759479782760882, 0.5762026663686868, 0.5851386281066929,
+            0.6023424727834, 0.6224012318616832, 0.6349951577963391,
+            0.6352127038584446, 0.6361159542649262, 0.6369708440504545,
+            0.6432382687009855, 0.6449485473328685, 0.6458541196589433,
+            0.6638401540497775, 0.6810857637187034, 0.6914374635530586,
+            0.7146862236370655, 0.7335122551062899, 0.7380305619344611,
+            0.7481552829167317, 0.7534333422153502, 0.7659278079221241,
+            0.7802925160056647, 0.7840515443802779, 0.7858175566560684,
+            0.7882952522603599, 0.7931210734787487, 0.8054471062280362,
+            0.8369260071883123, 0.8448121201845042, 0.8457743106122408,
+            0.8725394176743159, 0.8776084968191854, 0.8932892524100567,
+            0.8974703081229631, 0.9246294314690737, 0.9470450112295367,
+            0.9497456418201979, 0.9599420128556164, 0.9777130042139013,
+            0.9913972371243881, 0.9930411737585775, 0.9963741185277734,
+            0.9971933068336024])
+        double_raster_path = os.path.join(
+            self.workspace_dir, 'double_raster.tif')
+        n_length = 10
+        double_raster = gtiff_driver.Create(
+            double_raster_path, n_length, n_length, 1, gdal.GDT_Float32,
+            options=['TILED=YES', 'BIGTIFF=YES', 'COMPRESS=LZW',
+                     'BLOCKXSIZE=16', 'BLOCKYSIZE=16'])
+        double_raster.SetProjection(srs.ExportToWkt())
+        double_raster.SetGeoTransform([0.0, 1.0, 0.0, 0.0, 0.0, -1.0])
+        double_band = double_raster.GetRasterBand(1)
+        double_band.SetNoDataValue(-1)
+        double_band.WriteArray(array.reshape((n_length, n_length)))
+        double_raster.FlushCache()
+        double_band = None
+        double_raster = None
+
+        expected_float_percentiles = [
+            array[0], array[23], array[73], array[99], array[99]]
+        actual_percentiles = pygeoprocessing.disk_based_percentile(
+            (double_raster_path, 1), self.workspace_dir, percentile_cutoffs)
+        numpy.testing.assert_almost_equal(
+            actual_percentiles, expected_float_percentiles)
+
+    def test_percentile_unsupported_type(self):
+        """PGP: test percentile with unsupported type."""
+        srs = osr.SpatialReference()
+        srs.ImportFromEPSG(4326)
+        gtiff_driver = gdal.GetDriverByName('GTiff')
+        percentile_cutoffs = [0.0, 22.5, 72.1, 99.0, 100.0]
+        cdouble_raster_path = os.path.join(
+            self.workspace_dir, 'cdouble_raster.tif')
+        n_length = 10
+        cdouble_raster = gtiff_driver.Create(
+            cdouble_raster_path, n_length, n_length, 1, gdal.GDT_CFloat32,
+            options=['TILED=YES', 'BIGTIFF=YES', 'COMPRESS=LZW',
+                     'BLOCKXSIZE=16', 'BLOCKYSIZE=16'])
+        cdouble_raster.SetProjection(srs.ExportToWkt())
+        cdouble_raster.SetGeoTransform([0.0, 1.0, 0.0, 0.0, 0.0, -1.0])
+        cdouble_band = cdouble_raster.GetRasterBand(1)
+        cdouble_band.SetNoDataValue(-1)
+        cdouble_band = None
+        cdouble_raster = None
+
+        with self.assertRaises(ValueError) as cm:
+            _ = pygeoprocessing.disk_based_percentile(
+                (cdouble_raster_path, 1), self.workspace_dir,
+                percentile_cutoffs)
+        expected_message = 'Cannot process raster type'
+        actual_message = str(cm.exception)
+        self.assertTrue(expected_message in actual_message, actual_message)
