8c02c921416332cfd8b655d89abd3cf9632fcf75
Rich Sharp <richpsharp@gmail.com>
2020-05-07 11:43:50 -0700

    re #24 removing check for version in __init__.py

--- src/pygeoprocessing/__init__.py
+++ src/pygeoprocessing/__init__.py
@@ -9,27 +9,9 @@ import types
 from . import geoprocessing
 from .geoprocessing_core import calculate_slope
 from .geoprocessing_core import raster_band_percentile
-import pkg_resources
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.addHandler(logging.NullHandler())  # silence logging by default
-
-try:
-    __version__ = pkg_resources.get_distribution(__name__).version
-except pkg_resources.DistributionNotFound:
-    # Package is not installed, so the package metadata is not available.
-    # This should only happen if a package is importable but the package
-    # metadata is not, as might happen if someone copied files into their
-    # system site-packages or they're importing this package from the CWD.
-    raise RuntimeError(
-        "Could not load version from installed metadata.\n\n"
-        "This is often because the package was not installed properly. "
-        "Ensure that the package is installed in a way that the metadata is "
-        "maintained.  Calls to ``pip`` and this package's ``setup.py`` "
-        "maintain metadata.  Examples include:\n"
-        "  * python setup.py install\n"
-        "  * python setup.py develop\n"
-        "  * pip install <distribution>")
 
 __all__ = ('calculate_slope', 'raster_band_percentile')
 for attrname in dir(geoprocessing):
