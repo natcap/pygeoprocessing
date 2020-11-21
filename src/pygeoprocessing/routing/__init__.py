aeab1227b82a68d470d65e6b0f99dfeef476953b
James Douglass <jamesdouglassusa@gmail.com>
2020-11-20 16:36:52 -0800

    Adding __all__ to routing subpackage. RE:#47

--- src/pygeoprocessing/routing/__init__.py
+++ src/pygeoprocessing/routing/__init__.py
@@ -1,2 +1,25 @@
-from pygeoprocessing.routing.routing import *
-from pygeoprocessing.routing.watershed import *
+from pygeoprocessing.routing.routing import (
+    fill_pits,
+    fill_pits,
+    flow_dir_d8,
+    flow_accumulation_d8,
+    flow_dir_mfd,
+    flow_accumulation_mfd,
+    distance_to_channel_d8,
+    distance_to_channel_mfd,
+    extract_streams_mfd)
+from pygeoprocessing.routing.watershed import (
+    delineate_watersheds_d8)
+
+__all__ = (
+    'fill_pits',
+    'fill_pits',
+    'flow_dir_d8',
+    'flow_accumulation_d8',
+    'flow_dir_mfd',
+    'flow_accumulation_mfd',
+    'distance_to_channel_d8',
+    'distance_to_channel_mfd',
+    'extract_streams_mfd',
+    'delineate_watersheds_d8'
+)
