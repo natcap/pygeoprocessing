74b50da6deae6f23460e6fdad2e172f9c3c08c42
James Douglass <jamesdouglassusa@gmail.com>
2022-03-03 10:41:37 -0800

    Handling errors when loading the version string.
    A nonfunctional version string shouldn't prevent pygeoprocessing from
    loading.  RE:#243

--- src/pygeoprocessing/__init__.py
+++ src/pygeoprocessing/__init__.py
@@ -6,14 +6,27 @@ import logging
 import sys
 import types
 
+import pkg_resources
+import pkg_resources.extern.packaging.version
+
 from . import geoprocessing
 from .geoprocessing import ReclassificationMissingValuesError
 from .geoprocessing_core import calculate_slope
 from .geoprocessing_core import raster_band_percentile
-import pkg_resources
-__version__ = pkg_resources.get_distribution(__name__).version
 
-__all__ = ('calculate_slope', 'raster_band_percentile', 
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except (pkg_resources.extern.packaging.version.InvalidVersion,
+        DeprecationWarning):
+    # InvalidVersion is raised when our version string isn't PEP440.
+    # If there was an error, just skip the version string.
+    # Not having a version string shouldn't change how pygeoprocessing behaves
+    # or whether it imports.
+    # DeprecationWarning sometimes happens when we have an invalid version
+    # string as well.
+    __version__ = 'unknown'
+
+__all__ = ('calculate_slope', 'raster_band_percentile',
            'ReclassificationMissingValuesError')
 for attrname in dir(geoprocessing):
     attribute = getattr(geoprocessing, attrname)
