4a50b651c3c71fac76b6afe1e1cf87586979c4d8
James Douglass <jdouglass@stanford.edu>
2022-12-19 20:02:11 -0600

    Fixing a test in response to a deprecationwarning in numpy.
    RE:#232

--- tests/test_geoprocessing.py
+++ tests/test_geoprocessing.py
@@ -2380,7 +2380,7 @@ class TestGeoprocessing(unittest.TestCase):
 
     def test_raster_calculator_signed_byte(self):
         """PGP.geoprocessing: test that signed byte pixels interpreted."""
-        pixel_array = numpy.ones((128, 128), numpy.byte)
+        pixel_array = numpy.ones((128, 128), numpy.uint8)
         # ArcGIS will create a signed byte raster with an unsigned value of 255
         # that actually is supposed to represent -1, even though the nodata
         # value will be set as -1.
