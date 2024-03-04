e2bee912b0e27830f9b3319d6747dc2152d805df
davemfish <davemfish@gmail.com>
2024-03-04 17:24:41 -0500

    do not check if file exists before letting gdal open it. #375

--- src/pygeoprocessing/geoprocessing.py
+++ src/pygeoprocessing/geoprocessing.py
@@ -3895,8 +3895,6 @@ def get_gis_type(path):
         ``pygeoprocessing.RASTER_TYPE``, or ``pygeoprocessing.VECTOR_TYPE``.
 
     """
-    if not os.path.exists(path):
-        raise ValueError(f"{path} does not exist.")
     from pygeoprocessing import UNKNOWN_TYPE
     gis_type = UNKNOWN_TYPE
     gis_raster = gdal.OpenEx(path, gdal.OF_RASTER)
