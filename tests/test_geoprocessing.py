7c3597bae8aaff62748bbb15978fd344a6578650
Emily Soth <esoth@stanford.edu>
2024-05-20 12:45:37 -0700

    test conditionally on gdal version

--- tests/test_geoprocessing.py
+++ tests/test_geoprocessing.py
@@ -14,6 +14,7 @@ import types
 import unittest
 import unittest.mock
 import warnings
+from pkg_resources import packaging
 
 import numpy
 import packaging.version
@@ -2788,11 +2789,18 @@ class TestGeoprocessing(unittest.TestCase):
             self.workspace_dir, 'target_raster.tif')
         target_nodata = -1
         target_pixel_type = gdal.GDT_Int16
-        with self.assertRaises(ValueError) as cm:
-            pygeoprocessing.create_raster_from_vector_extents(
-                source_vector_path, target_raster_path, _DEFAULT_PIXEL_SIZE,
-                target_nodata, target_pixel_type)
-        self.assertIn('Invalid value for GDALDataType', str(cm.exception))
+        if packaging.version.parse(gdal.__version__) < packaging.version.parse('3.3.0'):
+            # older versions of GDAL raise a generic exception that's much less helpful
+            with self.assertRaises(Exception):
+                pygeoprocessing.create_raster_from_vector_extents(
+                    source_vector_path, target_raster_path, _DEFAULT_PIXEL_SIZE,
+                    target_nodata, target_pixel_type)
+        else:
+            with self.assertRaises(ValueError) as cm:
+                pygeoprocessing.create_raster_from_vector_extents(
+                    source_vector_path, target_raster_path, _DEFAULT_PIXEL_SIZE,
+                    target_nodata, target_pixel_type)
+            self.assertIn('Invalid value for GDALDataType', str(cm.exception))
 
     def test_create_raster_from_vector_extents_odd_pixel_shapes(self):
         """PGP.geoprocessing: create raster vector ext. w/ odd pixel size."""
